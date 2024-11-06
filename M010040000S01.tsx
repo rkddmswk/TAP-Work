71c081cab9b1aa5dd89e69bb32261d54efae37d3
rkddmswk <117643565+rkddmswk@users.noreply.github.com>
2024-11-06 17:20:24 +0900

    Update M010040000S01.tsx

--- M010040000S01.tsx
+++ M010040000S01.tsx
@@ -1,3 +1,15 @@
 Axios 영역
 1) await apiCommon
 -  pathUrl을 따로 설정해주지 않으면 defaultUrl로 들어오고 따로 설정해서 보내주면 해당로직으로 작동한다.
+
+
+
+gridSet 
+1) validation 체크하기
+- validationKey : true 설정
+- cellEditorParams: {
+  validationParams: {
+    메세지 설정, 로직설정
+  }
+}
+- valueSetter 로직 설정
