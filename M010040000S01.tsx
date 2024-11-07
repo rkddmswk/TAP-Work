6cc04586bf3c5ecd356adb53ee9af006a0987378
rkddmswk <117643565+rkddmswk@users.noreply.github.com>
2024-11-07 14:58:14 +0900

    Update M010040000S01.tsx

--- M010040000S01.tsx
+++ M010040000S01.tsx
@@ -7,9 +7,21 @@ Axios 영역
 gridSet 
 1) validation 체크하기
 - validationKey : true 설정
-- cellEditorParams: {
-  validationParams: {
-    메세지 설정, 로직설정
+- validationParams : 
+ex) cellEditorParams : {
+    validationParams : {
+      메세지 설정, 로직설정
+    }
   }
-}
-- valueSetter 로직 설정
+- valueSetter 설정
+ex) params.data[params.colDef.field] = params.newValu;
+    return true 
+=> 이 로직을 넣어줘야 valueSetter 할때 값을 입력할수있음
+
+2) params
+- api: 전체 그리드의 값
+- colDef: 선택되어 있는 필드 속성
+- column: 각 세로마다 정의되어 있는 , 열
+- newValue: 수정된 후의 값
+- oldValue: 수정하기 전의 값
+  
